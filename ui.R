53aee2dac84f1993480a6a6d230811b64877055e
caesared <f745606383@gmail.com>
2018-12-04 22:44:01 -0800

    Add a tab for leaflet map page, change fluid page to navbarpage

--- ui.R
+++ ui.R
@@ -1,27 +1,42 @@
 library(shiny)
+library(leaflet)
 
-ui <- fluidPage(
-    titlePanel('Exploring real estate price variations in WA'),
-    sidebarLayout(
-        sidebarPanel(
-            helpText('See sales only, rent only or both?'),
-            radioButtons('datasetFilter', 'Dataset filter',
-                         c('Show all neighborhoods'='none',
-                           'Have sales price data'='sales',
-                           'Have rental price data'='rent',
-                           'Have both datasets'='both'),
-                         selected='both'),
-            uiOutput('neighborhoodOut'),
-            uiOutput('timeRangeOut')
-        ),
-        mainPanel(
-            tabsetPanel(
-                type='tabs',
-                tabPanel(
-                    'Rental price fluctuation',
-                    plotOutput('rentPlot')
-                ),
-                tabPanel(
-                    'Sales price fluctutation',
-                    plotOutput('salesPlot')
-                )))))
+ui <- navbarPage('Exploring real estate price variations in WA', 
+                  id = "conditionedPanels",
+                  tabPanel("Real Estate Statistics",
+                            sidebarLayout(
+                            sidebarPanel(
+                                  helpText('See sales only, rent only or both?'),
+                                  radioButtons('datasetFilter', 'Dataset filter',
+                                               c('Show all neighborhoods'='none',
+                                                 'Have sales price data'='sales',
+                                                 'Have rental price data'='rent',
+                                                 'Have both datasets'='both'),
+                                               selected='both'),
+                                  uiOutput('neighborhoodOut'),
+                                  uiOutput('timeRangeOut')
+                            ),
+                  
+                            mainPanel(
+                                tabsetPanel(
+                                    type='tabs',
+                                    tabPanel(
+                                        'Rental price fluctuation',
+                                        plotOutput('rentPlot')
+                                    ),
+                                    tabPanel(
+                                        'Sales price fluctutation',
+                                        plotOutput('salesPlot')
+                                    )
+                  
+                                )
+                            )
+                        )
+                  ),
+                    
+                  tabPanel("Leaflet Map",
+                           textOutput("Map goes here")
+                  )
+                    
+)
+
